e51f81272b96857d2f7258cc71ebda07c91d22e8
Andrew Hetmanenko <andrew.hetmanenko@gmail.com>
2019-12-12 15:36:46 +0200

    Updates

---
+++ precache-manifest.15860b058f94cae6c8a1e61d8bdd7ff8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572fc59e2246d37361d42bb85b913bc4",
+    "url": "/csv-explorer/index.html"
+  },
+  {
+    "revision": "d3ae5b0c4570e65a968b",
+    "url": "/csv-explorer/static/css/main.d4052cf6.chunk.css"
+  },
+  {
+    "revision": "520f07129c964ba93cfb",
+    "url": "/csv-explorer/static/js/2.95617291.chunk.js"
+  },
+  {
+    "revision": "103ecd49f611c6e8e78b65f122bb34ab",
+    "url": "/csv-explorer/static/js/2.95617291.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3ae5b0c4570e65a968b",
+    "url": "/csv-explorer/static/js/main.fd9f6490.chunk.js"
+  },
+  {
+    "revision": "7a5898b76903a0e767c9",
+    "url": "/csv-explorer/static/js/runtime-main.fd19f9df.js"
+  }
+]);
